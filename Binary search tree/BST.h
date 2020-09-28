91db7ca369a2d7971093d2744c21dfbe4da91ecf
Conzxy <1967933844@qq.com>
2020-09-28 21:10:06 +0800

    BinarySearchTree

--- Binary search tree/BST.h
+++ Binary search tree/BST.h
@@ -1,195 +1,61 @@
-#ifndef BST_H
-#define BST_H
+//
+// Created by 谁敢反对宁宁，我就打爆他狗头！ on 2020/9/25.
+//
+
+#ifndef UNTITLED_BST_H
+#define UNTITLED_BST_H
 
 #include <iostream>
-#include <string>
-#include <cstdlib>
+#include <utility>
 
-template<typename T>
-struct BST_Node {
-	T data;		//结点数据
-	BST_Node<T>* lchild, * rchild;		//左右子树
+namespace zxy{
+    template<typename elem>
+    class BinarySearchTree{
+    public:
+        BinarySearchTree();
+        //Big Five
+        BinarySearchTree(const BinarySearchTree&);
+        BinarySearchTree(BinarySearchTree&&) noexcept;
+        ~BinarySearchTree();
 
-	//构造函数
-	BST_Node():lchild(nullptr),rchild(nullptr){}
-	BST_Node(T d,BST_Node<T> *l=nullptr,BST_Node<T> *r=nullptr):data(d),lchild(l),rchild(r){}
-};
+        const elem& findMin()const;
+        const elem& findMax()const;
+        bool contains(const elem&)const;
+        bool isEmpty()const;
+        void printTree(std::ostream &os=std::cout)const;
 
-template<typename T>
-class BST {
-public:
-	//=========构造函数和析构函数=========//
-	BST() :root(nullptr)
-	{
-		T x;
-		while (std::cin >> x)
-		{
-			Insert(x, root);
-			if (getchar() == '\n') break;
-		}
-	}
-	/*BST(T value) :root(nullptr), RefValue(value)
-	{
-		T x;
-		std::cin >> x;
-		while (x != RefValue)
-		{
-			Insert(x, root);
-			std::cin >> x;
-		}
-	}*/
+        void makeEmpty();
+        void insert(const elem&);
+        void insert(elem&&);
+        void remove(const elem&);
 
-	~BST() { Destroy(root); }
+        decltype(auto) operator=(const BinarySearchTree&);
+        decltype(auto) operator=(BinarySearchTree&&);
 
-	//=========插入=========//
-	bool Insert(T x) { return Insert(x, root); }
+    private:
+        struct BinaryNode{
+            elem element;
+            BinaryNode* left;
+            BinaryNode* right;
 
-	//=========删除=========//
-	bool Remove(T x) { return Remove(x, root); }
+            BinaryNode(const elem& theElem,BinaryNode* lt,BinaryNode* rt)
+                    :element(theElem),left(lt),right(rt){}
+            BinaryNode(elem&& theElem,BinaryNode* lt,BinaryNode* rt)
+                    :element(std::move(theElem)),left(lt),right(rt){}
+        };
 
-	//=========搜索=========//
-	bool Search(T x) { return (Search(x, root)!= nullptr) ? true : false; }
+        BinaryNode* root;
 
-	//=========中序遍历=========//
-	void InOrder() { InOrder(root); }
-protected:
-	bool Insert(T, BST_Node<T>*&);
-	bool Remove(T, BST_Node<T>*&);
-	BST_Node<T>* Search(T, BST_Node<T>*);
-	void InOrder(BST_Node<T>*);
-	void Destroy(BST_Node<T> *&);
-private:
-	//T RefValue;			//终止标志
-	BST_Node<T>* root;		//根节点
-};
-
-//插入结点
-template<typename T>
-bool BST<T>::Insert(T x, BST_Node<T> *&ptr)
-{
-	if (!ptr)
-	{
-		ptr = new BST_Node<T>(x);
-		if (!ptr)
-		{
-			std::cout << "分配内存失败！";
-			exit(1);
-		}
-		return true;
-	}
-	else if (x < ptr->data)		//进入左树
-	{
-		Insert(x, ptr->lchild);
-	}
-	else if (x > ptr->data)		//进入右树
-	{
-		Insert(x, ptr->rchild);
-	}
-	else
-	{
-		return false;
-	}
+        void insert(const elem&,BinaryNode* &);
+        void insert(elem&&,BinaryNode* &);
+        void remove(const elem&,BinaryNode* &);
+        BinaryNode* findMin(BinaryNode* )const;
+        BinaryNode* findMax(BinaryNode* )const;
+        bool contains(const elem&,BinaryNode*)const;
+        void makeEmpty(BinaryNode*);
+        void printTree(BinaryNode *,std::ostream&)const;
+        BinaryNode* clone(BinaryNode*)const;
+    };
 }
 
-//删除结点
-template<typename T>
-bool BST<T>::Remove(T x, BST_Node<T>*& ptr)
-{
-	BST_Node<T>* temp=nullptr;
-	if (ptr)
-	{
-		if (x<ptr->data)
-		{
-			Remove(x, ptr->lchild);
-		}
-		else if (x>ptr->data)
-		{
-			Remove(x, ptr->rchild);
-		}
-		else if (ptr->lchild && ptr->rchild)
-		{
-			temp = ptr->rchild;
-			while (temp->lchild)
-			{
-				temp = temp->lchild;
-			}
-			ptr->data = temp->data;
-			Remove(ptr->data, ptr->rchild);
-		}
-		else
-		{
-			if (!ptr->lchild)
-			{
-				ptr= ptr->rchild;
-			}
-			else if (!ptr->rchild)
-			{
-				ptr = ptr->lchild;
-			}
-			delete temp;
-			temp = nullptr;		//防止野指针的产生
-			return true;
-		}
-
-	}
-	else
-	{
-		return false;
-	}
-}
-
-//搜索结点并返回该节点地址
-template<typename T>
-BST_Node<T>* BST<T>::Search(T x, BST_Node<T>* ptr)
-{
-	if (!ptr)
-	{
-		return nullptr;
-	}
-	else if (x < ptr->data)	//进入左树搜索
-	{
-		return Search(x, ptr->lchild);
-	}
-	else if (x > ptr->data)	//进入右树搜索
-	{
-		return Search(x, ptr->rchild);
-	}
-	else      //找到返回
-	{
-		return ptr;
-	}
-}
-
-//中序遍历
-template<typename T>
-void BST<T>::InOrder(BST_Node<T>* ptr)
-{
-	if (ptr) {
-		InOrder(ptr->lchild);
-		std::cout << ptr->data << " ";
-		InOrder(ptr->rchild);
-	}
-
-}
-
-//删除以root为根的二叉搜索树（子树）
-template<typename T>
-void BST<T>::Destroy(BST_Node<T>*& root)
-{
-	if (!root)
-	{
-		return;
-	}
-	if (root->lchild)	//销毁左树
-	{
-		Destroy(root->lchild);
-	}
-	if (root->rchild)	//销毁右树
-	{
-		Destroy(root->rchild);
-	}
-	delete root;		//删除根节点
-	root = nullptr;
-}
-
-#endif
+#endif //UNTITLED_BST_H
---
+++ Binary search tree/BST_impl.h
@@ -0,0 +1,223 @@
+//
+// Created by 谁敢反对宁宁，我就打爆他狗头！ on 2020/9/27.
+//
+
+#ifndef UNTITLED_BST_IMPL_H
+#define UNTITLED_BST_IMPL_H
+
+#include "BST.h"
+
+namespace zxy{
+    //template alias of BinaryNode
+    template<typename T>
+    using BNode=typename BinarySearchTree<T>::BinaryNode;
+
+    template<typename T>
+    BinarySearchTree<T>::BinarySearchTree()
+            :root(nullptr){}
+
+    template<typename T>
+    BinarySearchTree<T>::BinarySearchTree(BinarySearchTree const& rhs)
+            :root(nullptr){
+        clone(rhs.root);
+    }
+
+    template<typename T>
+    BinarySearchTree<T>::BinarySearchTree(BinarySearchTree&& rhs) noexcept
+            :root (std::move(rhs.root))
+    {}
+
+    template<typename T>
+    BinarySearchTree<T>::~BinarySearchTree(){
+        makeEmpty();
+    }
+
+    template<typename T>
+    decltype(auto) BinarySearchTree<T>::operator=(BinarySearchTree<T> const& rhs)
+    {
+        auto tmp=rhs.root;
+        std::swap(root,rhs.root);
+        return *this;
+    }
+
+    template<typename T>
+    decltype(auto) BinarySearchTree<T>::operator=(BinarySearchTree<T>&& rhs)
+    {
+        std::swap(root,rhs.root);
+        rhs.root=nullptr;
+        return *this;
+    }
+
+    template<typename T>
+    bool BinarySearchTree<T>::contains(const T& item,BinaryNode* t)const
+    {
+        if(t==nullptr)
+            return false;
+        else if(t->element<item)
+            return contains(item,t->right);
+        else if(item<t->element)
+            return contains(item,t->left);
+        else
+            return true;
+    }
+
+    template<typename T>
+    bool BinarySearchTree<T>::contains(const T& item)const{
+        return contains(item,root);
+    }
+
+    template<typename T>
+    const T& BinarySearchTree<T>::findMin()const{
+        return {findMin(root)->element};
+    }
+
+    template<typename T>
+    typename BinarySearchTree<T>::BinaryNode*
+    BinarySearchTree<T>::findMin(BinaryNode *t)const
+    {
+        if(t==nullptr)
+            return nullptr;
+        else if(t->left!=nullptr)
+            return findMin(t->left);
+        else
+            return t;
+    }
+
+    template<typename T>
+    const T& BinarySearchTree<T>::findMax()const
+    {
+        return {findMax(root)->element};
+    }
+
+//从这个函数就可以明白为什么findMax要提供一个recursive function作为接口，因为要保证原件不被修改
+    template<typename T>
+    typename BinarySearchTree<T>::BinaryNode*  BinarySearchTree<T>::findMax(BinaryNode* t)const
+    {
+        if(t!=nullptr)
+        {
+            while(t->right!=nullptr)
+                t=t->right;
+        }
+        return t;
+    }
+
+    template<typename T>
+    void BinarySearchTree<T>::insert(T const& item,BinaryNode* &t)
+    {
+        if(t==nullptr)
+            t=new BinaryNode{item,nullptr,nullptr};
+        else if(item<t->element)
+            insert(item,t->left);
+        else if(t->element<item)
+            insert(item,t->right);
+        else
+            ;   //duplicate;do nothing
+    }
+
+    template<typename T>
+    void BinarySearchTree<T>::insert(T&& item,BinaryNode* &t)
+    {
+        if(t==nullptr)
+            t=new BinaryNode{std::move(item),nullptr,nullptr};
+        else if(item<t->element)
+            insert(std::move(item),t->left);
+        else if(t->element<item)
+            insert(std::move(item),t->right);
+        else
+            ;
+    }
+
+    template<typename T>
+    void BinarySearchTree<T>::insert(T const& item)
+    {
+        insert(item,root);
+    }
+
+    template<typename T>
+    void BinarySearchTree<T>::insert(T&& item)
+    {
+        insert(std::move(item),root);
+    }
+
+    template<typename T>
+    void BinarySearchTree<T>::remove(T const& item)
+    {
+        remove(item,root);
+    }
+
+//remove应该是这里面最难的一个了
+    template<typename T>
+    void BinarySearchTree<T>::remove(T const& item,BinaryNode* &t)
+    {
+        if(t==nullptr)
+            return ;
+        if(item<t->element)
+            remove(item,t->left);
+        else if(t->element<item)
+            remove(item,t->right);
+        else if(t->left!=nullptr&&t->right!=nullptr)    //two child
+        {
+            t->element=findMin(t->right)->element;
+            //the element of the smallest entry in right subtree replace the element of parent
+            remove(t->element,t->right);
+            //remove the smallest entry in right subtree,ensure property of binary search tree normal
+        }
+        else    //one child or single node
+        {
+            auto oldNode = t;
+            t = (t->right != nullptr) ? t->right : t->left; //child replace oldNode
+            delete oldNode;
+        }
+    }
+
+    template<typename T>
+    void BinarySearchTree<T>::makeEmpty(BinaryNode* t){
+        if(t!=nullptr){
+            makeEmpty(t->left);
+            makeEmpty(t->right);
+            delete t;
+        }
+        t=nullptr;
+    }
+
+    template<typename T>
+    void BinarySearchTree<T>::makeEmpty()
+    {
+        makeEmpty(root);
+    }
+
+    template<typename T>
+    bool BinarySearchTree<T>::isEmpty()const {
+        return root!=nullptr;
+    }
+
+    template<typename T>
+    void BinarySearchTree<T>::printTree(BinaryNode* t,std::ostream& os)const{
+        if(t!=nullptr)
+        {
+            printTree(t->left,os);
+            os<<t->element<<" ";
+            printTree(t->right,os);
+        }
+    }
+
+    template<typename T>
+    void BinarySearchTree<T>::printTree(std::ostream& os) const{
+        printTree(root,os);
+    }
+
+    template<typename T>
+    typename BinarySearchTree<T>::BinaryNode*
+    BinarySearchTree<T>::clone(BinaryNode *t)const
+    {
+        if(t!=nullptr)
+        {
+            return new BinaryNode{t->element,clone(t->left),clone(t->right)};
+        }
+        else
+            return nullptr;
+    }
+}
+
+
+#endif //UNTITLED_BST_IMPL_H
